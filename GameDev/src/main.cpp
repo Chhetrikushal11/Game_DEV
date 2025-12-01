9ea4d4e65aa3fb091c7f529af3b0da55ac19c8bb
KUSHAL <chhetrikushal11@gmail.com>
2025-12-01 13:31:58 -0500

    Create the first window

--- GameDev/src/main.cpp
+++ GameDev/src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+
 
 int main()
 {
-	std::cout << "Hello World!!" << std::endl;
+	if (!glfwInit()) return -1;
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "TITLE WINDOW", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Error creating window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// to define window position
+	glfwSetWindowPos(window, 2000, 150);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	
 	return 0;
 }
